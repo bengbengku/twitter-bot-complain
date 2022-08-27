cd901bfb0a6602f7016dc6ccb571a1f551a4200c
Anggiat Benget Napitupulu <anggiatbenget70@gmail.com>
2022-08-27 23:29:47 +0700

    Twitter Bot Complaint Internet Provider

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ s = Service(CHROME_DRIVER_PATH)
 options = Options()
 options.add_argument("start-maximized")
 
+
 class InternetSpeedTwitterBot:
     def __init__(self):
         self.driver = webdriver.Chrome(service=s, options=options)
@@ -25,18 +26,19 @@ class InternetSpeedTwitterBot:
     def get_internet_speed(self):
         try:
             self.driver.get("https://www.speedtest.net/id")
-            button_speed = self.driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[1]/a/span[4]')
+            button_speed = self.driver.find_element(
+                By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[1]/a/span[4]')
             button_speed.click()
             time.sleep(50)
         except:
             pass
         finally:
-            text_down = self.driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text
-            text_up = self.driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[3]/div/div[2]/span').text
+            text_down = self.driver.find_element(
+                By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text
+            text_up = self.driver.find_element(
+                By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[3]/div/div[2]/span').text
             self.down = float(text_down)
             self.up = float(text_up)
-
-
 
     def tweet_at_provider(self):
         self.driver.get("https://twitter.com/i/flow/login")
@@ -47,7 +49,7 @@ class InternetSpeedTwitterBot:
                 EC.presence_of_element_located((By.NAME, 'text'))
             )
 
-            input_username.send_keys("anggiatbenget")
+            input_username.send_keys("USERNAME")
             input_username.send_keys(Keys.ENTER)
             time.sleep(3)
         finally:
@@ -55,15 +57,17 @@ class InternetSpeedTwitterBot:
                 EC.presence_of_element_located((By.NAME, 'password'))
             )
 
-            input_username.send_keys("Maladewa1234")
+            input_username.send_keys("YOUR PASSWORD")
             input_username.send_keys(Keys.ENTER)
 
         try:
             tweet = WebDriverWait(self.driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/label/div[1]/div/div/div/div/div[2]/div/div/div/div/span'))
+                EC.presence_of_element_located(
+                    (By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/label/div[1]/div/div/div/div/div[2]/div/div/div/div/span'))
             )
         finally:
             tweet.send_keys(f"{text_template}")
+
 
 main_ = InternetSpeedTwitterBot()
 
